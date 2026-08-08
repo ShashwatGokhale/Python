719be44d47c55644574130298e3faaf360967658
ShashwatGokhale <shashwatgokhale02@gmail.com>
2026-08-08 23:12:47 +0530

    python

---
+++ list.py
@@ -0,0 +1,25 @@
+
+#FOR LOOP:-   For loops is used to iterate over a sequence such as a list, tuple, string or range. It executes a block of code once for each item in the sequence.
+# for loop throgh list
+list=["apple", "banana", "orange"]
+for i in list:
+     print(i)
+
+#loop through index no.
+list= ["apple", "banana", "orange"]
+for i in range(len(list)):
+     print(list[i])
+
+#WHILE LOOP: runs the block of code until the condition is true 
+
+list=  ["apple", "banana", "orange"]
+i= 0 
+while i< len(list):
+     print(list[i])
+     i=i+1
+
+cnt = 0
+while (cnt < 3):
+    print("Hello Geek")
+    cnt = cnt + 1
+ 
