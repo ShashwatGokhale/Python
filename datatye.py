cd58b51ff94db7697a3f8a5800aa03c8d14eac84
ShashwatGokhale <shashwatgokhale02@gmail.com>
2026-08-08 23:01:41 +0530

    python

---
+++ datatye.py
@@ -0,0 +1,32 @@
+#Built-in Data Types
+# Text type: str
+# Numeric types: int, float, complex
+# Sequence Type: List, Tuple, range
+# mapping: dict
+# set type: set, frozenset
+# boolean type: bool
+# Binary type: bytes, bytearray, memoryview
+
+# x = 5
+# y="sharad"
+# print(type(y))
+# x= "sharad khare" #str
+# x = 200 #int
+# x=20.5 #float
+# x =1j #complex
+# X ["sharad", "khare"] #List
+# x = ("sharad", "Khare") πτuple
+# X range(5) #range
+# x = {"name"; "sharad", "age": "50"} #dict
+# x = {"apple", "bapana", "orange"} #set
+x = True  #boolean
+x= "school" #bytes
+x = bytearray(5) #bytearry
+x = memoryview(bytes(3)) #memoryview
+
+#tye conversion
+#conversion of int to float
+x=10
+y=2.0
+a=float(x)
+rint(x)
